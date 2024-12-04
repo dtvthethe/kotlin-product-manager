6da8c83da8b6aa795feaec591d2fc2f00792e2ba
thedtv-1961 <dam.trieu.van.the@sun-asterisk.com>
2024-12-04 10:58:48 +0700

    map-dto-with-entity

---
+++ src/main/kotlin/org/dtvthe/kotlinproductmanager/config/AppConfig.kt
@@ -0,0 +1,13 @@
+package org.dtvthe.kotlinproductmanager.config
+
+import org.modelmapper.ModelMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class AppConfig {
+    @Bean
+    fun modelMapper(): ModelMapper {
+        return ModelMapper()
+    }
+}
--- src/main/kotlin/org/dtvthe/kotlinproductmanager/service/CategoryService.kt
+++ src/main/kotlin/org/dtvthe/kotlinproductmanager/service/CategoryService.kt
@@ -2,18 +2,26 @@ package org.dtvthe.kotlinproductmanager.service
 
 import org.dtvthe.kotlinproductmanager.dto.CategoryDTO
 import org.dtvthe.kotlinproductmanager.entity.Category
-import org.dtvthe.kotlinproductmanager.entity.Product
 import org.dtvthe.kotlinproductmanager.repository.CategoryRepository
+import org.modelmapper.ModelMapper
 import org.springframework.stereotype.Service
 
 @Service
-class CategoryService (private val categoryRepository: CategoryRepository) {
+class CategoryService(private val categoryRepository: CategoryRepository, private val modelMapper: ModelMapper) {
     fun findAll(): List<Category> = categoryRepository.findAll()
 
     fun create(categoryDTO: CategoryDTO) {
+        println(categoryDTO.toString())
+        val category = convertCategoryDTOToEntity(categoryDTO)
+        println(category.toString())
+
 //        val category = CategoryMapper.INSTANCE.toEntity(categoryDTO);
 //        val result = categoryRepository.save(category);
 
 //        println(result)
+    }
+
+    fun convertCategoryDTOToEntity(categoryDTO: CategoryDTO): Category {
+        return modelMapper.map(categoryDTO, Category::class.java)
     }
 }
